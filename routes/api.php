aadd6dad027303df43e88d6ec502ad3d26a66a2a
Diki Dwi Hermawan <dikidwihermawan@gmail.com>
2023-09-30 11:48:07 +0700

    add sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,42 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\MeController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Status\StatusController;
+use App\Http\Controllers\Users\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', LoginController::class);
+Route::post('/register', RegisterController::class);
+
+Route::middleware("auth:sanctum")->group(function () {
+    Route::get('/me', MeController::class);
+    Route::post('/logout', LogoutController::class);
+
+    // status
+
+    Route::get('/status', [StatusController::class, 'index']);
+    Route::post('/status', [StatusController::class, 'store']);
+
+    // comment status
+    Route::post('/status/{identifier}/comment', [StatusController::class, 'comment']);
+
+    // likes
+    Route::post('/status/{identifier}/likes', [StatusController::class, 'likes']);
+
+    // users
+
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/follow', [UserController::class, 'follow']);
+
+    // follow
+
+    Route::post('/users/following', [UserController::class, 'following']);
+
+
+    // profile
+    Route::get('/profile/{username}', [UserController::class, 'profile']);
 });
